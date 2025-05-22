a8a4782db7d8316dcabd303b8f1b1e6ae44f9502
Carmen <carmen.n.riggs@gmail.com>
2025-05-22 12:52:08 -0400

    Optimized logging

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,17 @@
 #include "signalGenerate.hpp"
 #include <cstdlib>
 #include <thread>
+#include <vector>
 #include <chrono>
 using namespace std::chrono;
 using namespace std;
+
+struct loggingData
+{
+    float filteredData;
+    double timeStamp;
+    float rawData;
+};
 
 int main(int argc, char *argv[])
 {
@@ -25,6 +33,7 @@ int main(int argc, char *argv[])
     float freqHz = std::stof(argv[5]);
     int period = (1.0 / samplingRateHz) * 1000;
     float signal[maxSamples];
+    std::vector<loggingData> dataReference;
 
     auto startTimeStamp = high_resolution_clock::now(); // Beginning the clock that times each instance for the variable timeStamp
 
@@ -40,6 +49,7 @@ int main(int argc, char *argv[])
         return 1;
     }
 
+    // Real Time loop
     for (size_t i = 0; i < numSamples; i++)
     {
         float filteredData = movingFilter.run(signal[i]); // Applying moving average filter onto noisy data set
@@ -48,11 +58,26 @@ int main(int argc, char *argv[])
         auto calculatedTimeStamp = duration_cast<milliseconds>(stopTimeStamp - startTimeStamp); // Getting milliseconds for decimal point
         double timeStamp = static_cast<double>(calculatedTimeStamp.count()) / 1000.0;           // casting to a double and converting to seconds
 
-        std::cout << timeStamp << ", " << signal[i] << ", " << filteredData << endl; // print to terminal
-        csvFile << timeStamp << ", " << signal[i] << ", " << filteredData << endl;   // print to .csv file
+        loggingData entry;
+        entry.filteredData = filteredData;
+        entry.timeStamp = timeStamp;
+        entry.rawData = signal[i];
+        dataReference.push_back(entry);
+
+        // Commented out for run-time optimization
+        //  std::cout << timeStamp << ", " << signal[i] << ", " << filteredData << endl; // print to terminal
+        //  csvFile << timeStamp << ", " << signal[i] << ", " << filteredData << endl;   // print to .csv file
 
         std::this_thread::sleep_for(std::chrono::milliseconds(period)); // delaying by the period
     }
+
+    // Post Processing the data Loop
+    for (const auto &entry : dataReference)
+    {
+        std::cout << entry.timeStamp << ", " << entry.rawData << ", " << entry.filteredData << endl; // print to terminal
+        csvFile << entry.timeStamp << ", " << entry.rawData << ", " << entry.filteredData << endl;   // print to .csv file
+    }
+
     csvFile.close();
     return 0;
 }
